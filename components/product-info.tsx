9780905e52e4bc84af39d1262ed0b8e1250f24af
sandeepsinghmehra <sandysmart017@gmail.com>
2023-08-05 21:40:51 +0530

    Update detail page ui

--- components/product-info.tsx
+++ components/product-info.tsx
@@ -3,7 +3,10 @@
 import { Product } from "@/types";
 import { Currency } from "@/components/ui/currency";
 import { Button } from "@/components/ui/button";
-import { ShoppingCart } from "lucide-react";
+import { Album, ShoppingCart, Terminal, Truck } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { Input } from "./ui/input";
+import Link from "next/link";
 
 interface InfoProps {
     data: Product
@@ -14,10 +17,13 @@ const Info: React.FC<InfoProps> = ({
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
-            <div className="mt-3 flex items-end justify-between">
+            <p className="text-muted-foreground text-sm">A perfect balance of exhilarating high-fidelity  audio  and effortless magic of AirPods and effortless magic of AirPods.</p>
+            <hr className="my-4" />
+            <div className="mt-3 flex flex-col items-start justify-between">
                 <p className="text-2xl text-gray-900">
                 <Currency value={data?.price.$numberDecimal} />
                 </p>
+                <p className="text-muted-foreground text-sm">Suggested Payments with 6 month special financing.</p>
             </div>
             <hr className="my-4" />
             <div className="flex flex-col gap-y-6">
@@ -32,6 +38,7 @@ const Info: React.FC<InfoProps> = ({
                 <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.colorId?.value }} />
                 </div>
             </div>
+            <hr className="my-4" />
             <div className="mt-10 flex items-center gap-x-3">
                 <Button
                     size={'lg'}
@@ -45,10 +52,37 @@ const Info: React.FC<InfoProps> = ({
                     size={'lg'}
                     variant={'outline'}
                     onClick={()=>{}} 
-                    className="space-x-2 text-teal-900 rounded-3xl"
+                    className="space-x-2 text-teal-900 rounded-3xl border-2 border-teal-900"
                 >
                     Add To Cart
                 </Button>
+            </div>
+            <div className="border rounded-md mt-4">
+                <Alert className="border-none">
+                    <Truck 
+                        color="red"
+                        className="h-4 w-4 text-red-600" 
+                    />
+                    <AlertTitle>Free Delivery</AlertTitle>
+                    <AlertDescription>
+                        <Input 
+                            type="text"
+                            placeholder="Enter your Postal code for Delivery Avaliability" 
+                            className="px-0 py-0 h-7 border-none focus:outline-none outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-transparent underline"
+                        />
+                    </AlertDescription>
+                </Alert>
+                <hr className="my-0" />
+                <Alert className="border-none">
+                    <Album  
+                        color="red" 
+                        className="h-4 w-4" 
+                    />
+                    <AlertTitle>Return Delivery</AlertTitle>
+                    <AlertDescription>
+                        Free 30days Delivery Returns. <Link href={'/'}>Details</Link>
+                    </AlertDescription>
+                </Alert>
             </div>
         </div>
     )
