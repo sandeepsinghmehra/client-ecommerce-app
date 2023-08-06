eb4abc07806229d991dedb697f6bb5e98ce7f073
sandeepsinghmehra <sandysmart017@gmail.com>
2023-08-06 10:35:28 +0530

    Added plus minus button in ui

--- components/product-info.tsx
+++ components/product-info.tsx
@@ -3,7 +3,7 @@
 import { Product } from "@/types";
 import { Currency } from "@/components/ui/currency";
 import { Button } from "@/components/ui/button";
-import { Album, ShoppingCart, Terminal, Truck } from "lucide-react";
+import { Album, Minus, Plus, ShoppingCart, Terminal, Truck } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 import { Input } from "./ui/input";
 import Link from "next/link";
@@ -18,15 +18,15 @@ const Info: React.FC<InfoProps> = ({
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
             <p className="text-muted-foreground text-sm">A perfect balance of exhilarating high-fidelity  audio  and effortless magic of AirPods and effortless magic of AirPods.</p>
-            <hr className="my-4" />
+            <hr className="my-5" />
             <div className="mt-3 flex flex-col items-start justify-between">
                 <p className="text-2xl text-gray-900">
                 <Currency value={data?.price.$numberDecimal} />
                 </p>
                 <p className="text-muted-foreground text-sm">Suggested Payments with 6 month special financing.</p>
             </div>
-            <hr className="my-4" />
-            <div className="flex flex-col gap-y-6">
+            <hr className="my-5" />
+            <div className="flex flex-col gap-y-4">
                 <div className="flex items-center gap-x-4">
                 <h3 className="font-semibold text-black">Size:</h3>
                 <div>
@@ -38,8 +38,31 @@ const Info: React.FC<InfoProps> = ({
                 <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.colorId?.value }} />
                 </div>
             </div>
-            <hr className="my-4" />
-            <div className="mt-10 flex items-center gap-x-3">
+            <hr className="my-5" />
+            <div className="flex flex-row">
+                <div className="flex flex-row items-center justify-evenly border rounded-3xl px-4 space-x-4 bg-gray-200">
+                    <Button
+                        size={'icon'}
+                        variant={'outline'}
+                        className="outline-none border-none bg-gray-200"
+                    >
+                        <Minus size={20} className="w-5 h-5" />
+                    </Button>
+                    <p className="text-muted-foreground">1</p>
+                    <Button 
+                        size={'icon'}
+                        variant={'outline'}
+                        className="outline-none border-none bg-gray-200"
+                    >
+                        <Plus size={20} className="w-5 h-5" />
+                    </Button>
+                </div>
+                <div className="flex flex-col ml-5">
+                    <p className="text-muted-foreground text-sm font-semibold">Only <span className="text-red-500">12 items</span> left!</p>
+                    <p className="text-muted-foreground text-sm font-semibold">Don't miss it</p>
+                </div>
+            </div>
+            <div className="mt-5 flex items-center gap-x-3">
                 <Button
                     size={'lg'}
                     variant={'default'}
@@ -57,7 +80,7 @@ const Info: React.FC<InfoProps> = ({
                     Add To Cart
                 </Button>
             </div>
-            <div className="border rounded-md mt-4">
+            <div className="border rounded-md mt-5">
                 <Alert className="border-none">
                     <Truck 
                         color="red"
