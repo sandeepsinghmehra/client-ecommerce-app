974240237ed1679c683ddab49bbc702b54ef0632
sandeepsinghmehra <sandysmart017@gmail.com>
2023-08-23 23:11:34 +0530

    Comment img tag for removing warning

--- components/signup/Signup.tsx
+++ components/signup/Signup.tsx
@@ -36,7 +36,7 @@ const Signup = () => {
             <div className={`mx-auto w-full max-w-lg bg-gray-200/50 rounded-xl p-10`}>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[60px]">
-                        <img src="/favicon.ico" alt="Logo" />
+                        {/* <img src="/favicon.ico" alt="Logo" /> */}
                     </span>
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight text-black">
