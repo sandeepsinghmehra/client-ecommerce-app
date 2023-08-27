cb88b9fc9c43bf007983b6ed07be6917810c1ef1
sandeepsinghmehra <sandysmart017@gmail.com>
2023-08-27 12:22:29 +0530

    update ui

--- app/(routes)/orders/[orderId]/page.tsx
+++ app/(routes)/orders/[orderId]/page.tsx
@@ -24,7 +24,7 @@ const OrderPage:React.FC<OrderPageProps> = async({
               <h2 className='text-sm text-gray-500 tracking-widest uppercase'>Shopcart.com</h2>
               <h1 className='text-gray-900 text-xl font-medium mb-4'>Order Id: <span className='text-gray-600'>#{order._id}</span></h1>
               <p className='leading-relaxed mb-4'>Your order has been successfully placed.</p>
-              <p> Your Payment Status is: <span className={order.status.toLowerCase() === 'paid'? 'text-green-700 font-bold': 'text-orange-400 font-bold'}>{order.status}</span></p>
+              <p className='leading-relaxed mb-4'> Your Payment Status is: <span className={order.status.toLowerCase() === 'paid'? 'text-green-700 font-bold': 'text-orange-400 font-bold'}>{order.status}</span></p>
               
               {order.orderItems.map((item:any)=>(<div key={item._id} className='flex flex-col md:flex-row md:justify-between border-t border-gray-200 py-2'>
                 <div className='flex items-center'>
@@ -46,8 +46,9 @@ const OrderPage:React.FC<OrderPageProps> = async({
                   <span className='text-gray-400 text-sm font-semibold'>Qty: {item.quantity}</span>
                 </div> 
               </div>))}
+              <hr className='bg-slate-200'/>
               <div className='flex flex-col items-end'>
-                <div className='font-medium text-2xl text-gray-700'>SubTotal: ${JSON.parse(order.paymentInfo).TXNAMOUNT}</div>
+                <div className='font-medium text-2xl text-gray-700 mt-7'>SubTotal: ${JSON.parse(order.paymentInfo).TXNAMOUNT}</div>
                 <div className='my-6'>
                   <Button
                     variant={'destructive'}
