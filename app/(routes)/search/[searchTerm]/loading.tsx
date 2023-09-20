0024ed99c7b290ef982c411be6bff42c61a0ffa1
sandeepsinghmehra <sandysmart017@gmail.com>
2023-09-20 11:54:17 +0530

    Added key in loading article tag for mapping

--- app/(routes)/search/[searchTerm]/loading.tsx
+++ app/(routes)/search/[searchTerm]/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
   return (
     <Container>
       <div className="w-full h-full p-8">
-            {[1,2,3,4].map((repeat)=>(<article className="mx-auto my-3 w-4/5 bg-transparent border rounded-xl" >
+            {[1,2,3,4].map((repeat, i)=>(<article className="mx-auto my-3 w-4/5 bg-transparent border rounded-xl" key={i}>
                 <div className="flex flex-row gap-4">
                     <div className="flex flex-col justify-center">
                         <Skeleton className="aspect-square rounded-xl" style={{width: '300px', height: '300px'}} />
